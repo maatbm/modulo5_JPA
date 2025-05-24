e405872284aae3b672a0e753a96844f7b59d1bde
maatbm <matheusmoraes010@outlook.com.br>
2025-05-24 10:10:19 -0300

    Finalizado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("modulo5_JPA");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.close();
+        entityManagerFactory.close();
     }
 }
